e1529d0c001a2bab4f93c6f89c64e4e6beb5737a
Idriss975 <idrissberchil@fedora.email>
2024-04-04 21:58:14 +0000

    Exclude gateway from scan + Add listing scanned machines + Fix code mistakes

--- arplover.sh
+++ arplover.sh
@@ -30,15 +30,16 @@ printf "\e[37;41m║                                                            
 printf "\e[37;41m╠═════════════════════════ BERCHIL Idriss ══════════════════════════════════╝ \033[0m\n"
 printf "║\n"
 #TODO: if $1 is a host (not network) disable this option and auto configure
-printf "╠═  1. Scan for IP Addresses (ARP Scan)\n"
+printf "╠═  1. Scan for available machine within your network (ARP Scan)\n"
 #TODO: Not allow 2nd & 3rd option without scanning first
-printf "╠═  2. Kill Network Access (ARP Spoof target only)\n"
+printf "╠═  2. Kill Network Access for scanned machines (ARP Spoof target only)\n"
 printf "╠═  3. Man In the Middle (ARP Spoof target & gateway)\n"
-printf "╠═  4. Protect yourself using ArpON\n"
-printf "╚═  5. Exit\n"
-printf "║\n"
-printf "╚═ 	Gateway is ${Gateway}, \n"
-printf "╚═ 	Target is $1. \n"
+printf "╠═  4. List Scanned machines\n"
+printf "╠═  5. Protect yourself using ArpON\n"
+printf "╚═  6. Exit\n"
+printf "\n"
+printf "╔═ 	Gateway is ${Gateway}, \n"
+printf "╠═ 	Target is $1. \n"
 printf "╚═ 	Interface is $2, \n"
 
 
@@ -50,16 +51,22 @@ do
 	case ${choice} in
 	1)
 		echo "Scanning ..."
-		Targets=$(sudo nmap -sn $1 2>/dev/null -oG - | grep "Up$" | awk '{printf "%s ", $2}')
+		Targets=$(sudo nmap -sn $1 --exclude ${Gateway} 2>/dev/null -oG - | grep "Up$" | awk '{printf "%s ", $2}')
 		
-		echo "Targets are:"
-		for i in $Targets
-		do
-		echo $i
-		done
+		if [[ ! ${#Targets[@]} -eq 0 ]]
+		then
+			echo "Targets are:"
+			for i in $Targets
+			do
+				echo $i
+			done
+		else
+			echo "No Targets found."
+		fi
 		;;
+
 	2)
-		if [[ ${#IPs[@]} -eq 0 ]]
+		if [[ ${#Targets[@]} -eq 0 ]]
 		then
 			printf "No targets "
 		fi
@@ -71,18 +78,26 @@ do
 			IPs+=$!
 		done
 		;;
+
 	3)
 	#Arp spoof target
 	#ARp spoof gateway as target
 	#open wireshark
 
 	;;
+
 	4)
+
+
+	;;
+
+	5)
 		printf "Running Arpon ...\n"
 		sudo arpon -D -d -i $2
 		printf "Arpon has been enabled as background daemon.\n"
 		;;
-	5)
+		
+	6)
 		if [[ ! ${#IPs[@]} -eq 0 ]] then
 			printf "Rearping...\n"
 		fi
@@ -90,7 +105,7 @@ do
 		for i in $IPs 
 		do
 			kill -SIGINT $i
-			sleep 5
+			sleep 3
 			sudo kill -SIGKILL $i
 		done
 
